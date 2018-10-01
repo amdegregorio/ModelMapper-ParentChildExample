8c19b7120798174e68f7de64e458e08239279768
amdegregorio <fake@donotspam.com>
2018-10-01 06:45:43 -0400

    initial add of empty initialized spring boot project

---
+++ src/main/java/com/amydegregorio/mmparentchildex/modelmapperparentchild/ModelMapperParentChildApplication.java
@@ -0,0 +1,12 @@
+package com.amydegregorio.mmparentchildex.modelmapperparentchild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelMapperParentChildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelMapperParentChildApplication.class, args);
+	}
+}
